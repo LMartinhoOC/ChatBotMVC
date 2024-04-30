db0aa0aedba3b62a46c50c99eb353787e9f60ef0
luiz.martinho <luizacacio3107@gmail.com>
2024-04-30 06:12:04 -0300

    primeiro commit

---
+++ ChatBotMVC/Models/ChaveAPIViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatBotMVC.Models
+{
+    public class ChaveAPIViewModel
+    {
+        [Required(ErrorMessage = "A Chave API é obrigatória!")]
+        public string ChaveAPI {  get; set; }
+    }
+}
---
+++ Entidades/Mensagem.cs
@@ -0,0 +1,18 @@
+﻿namespace Entidades
+{
+    public class Mensagem
+    {
+        public string Id { get; set; }
+        public string Content { get; set; }
+        public int Total_tokens { get; set; }
+        public DateTime Created { get; set; }
+
+        public override string ToString()
+        {
+            return $"MENSAGEM RECEBIDA - {Created}\n" +
+                   $"Id: {this.Id}\n" +
+                   $"Mensagem: {this.Content}\n" +
+                   $"Total de tokens gastos: {this.Total_tokens}\n";
+        }
+    }
+}
